1dee2b6f32d6971ce1899f30b7fa8293a5714e0c
Helfi Pangestu <hellfipangestu@gmail.com>
2021-10-18 23:18:41 +0700

    Add campaign services

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ func main() {
 	}
 
 	userRepository := user.NewRepository(db)
+	// campaignRepository := campaign.NewRepository(db)
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
 
